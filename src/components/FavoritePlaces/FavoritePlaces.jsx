d6f93a644a39f000af9f2801c5f9bf76dd1d718a
yonatankumsa <guitaristyona@gmail.com>
2022-12-02 20:54:13 -0500

    Dates used for fetch call are one month in the future instead of one year. This maximizes chance of hotel availability when searching

--- src/components/FavoritePlaces/FavoritePlaces.jsx
+++ src/components/FavoritePlaces/FavoritePlaces.jsx
@@ -1,29 +1,66 @@
 import { useState, useEffect } from 'react';
 import { markPlacesArr, jingPlacesArr, tirasPlacesArr } from './ourFavoritePlacesData'
+const axios = require("axios");
+const API_KEY = process.env.REACT_APP_BOOKING_API_KEY
 
-
-
-let today = new Date()
-let tomorrow = new Date()
-tomorrow.setDate(today.getDate() + 1)
+// Using dates one month in the future for checkin / checkout because some destinations were sold out of hotels today
+let checkIn = new Date(new Date().setMonth(new Date().getMonth() + 1))
+let checkOut = new Date(new Date().setMonth(new Date().getMonth() + 1))
+checkOut.setDate(checkIn.getDate() + 1)
 // This makes the format 'yyyy-mm-dd' for the axios fetch
-today = today.toISOString().slice(0, 10)
-tomorrow = tomorrow.toISOString().slice(0, 10)
-// console.log(today)
-// console.log(tomorrow)
-
-
+checkIn = checkIn.toISOString().slice(0, 10)
+checkOut = checkOut.toISOString().slice(0, 10)
+console.log(checkIn)
+console.log(checkOut)
 
 
 export default function FavoritePlaces() {
     const [markPlace, setMarkPlace] = useState({})
     const [jingPlace, setJingPlace] = useState({})
     const [tirasPlace, setTirasPlace] = useState({})
+    const [markHotels, setMarkHotels] = useState([])
+    const [jingHotels, setJingHotels] = useState([])
+    const [tirasHotels, setTirasHotels] = useState([])
 
     const getRandomPlace = (personArr, setState) => {
         let randomIndex = Math.floor(Math.random() * personArr.length)
         let personPlace = personArr[randomIndex]
         setState(personPlace)
+    }
+
+    // This function takes each of our randomPlace and uses the coordinates of that place to find hotels there
+    const getRandomHotels = (personPlace, setHotels) => {
+        const options = {
+            method: 'GET',
+            url: 'https://booking-com.p.rapidapi.com/v1/hotels/search-by-coordinates',
+            params: {
+                order_by: 'popularity',
+                adults_number: '2',
+                units: 'metric',
+                room_number: '1',
+                checkout_date: checkOut,
+                filter_by_currency: 'USD',
+                locale: "en-gb",
+                checkin_date: checkIn,
+                latitude: personPlace.latitude,
+                longitude: personPlace.longitude,
+                categories_filter_ids: 'class::2,class::4,free_cancellation::1',
+                page_number: '0',
+                include_adjacency: 'true'
+            },
+            headers: {
+                'X-RapidAPI-Key': API_KEY,
+                'X-RapidAPI-Host': 'booking-com.p.rapidapi.com'
+            }
+        };
+
+        axios.request(options).then(function (response) {
+            const hotels = response.data.result
+            console.log(hotels)
+            setHotels(hotels)
+        }).catch(function (error) {
+            console.error(error);
+        });
     }
 
     useEffect(() => {
@@ -43,7 +80,10 @@ export default function FavoritePlaces() {
                 <h4>Why He Loves it There:</h4>
                 <span>"{markPlace.testimonial}"</span>
                 <h4>Featured Accomodation:</h4>
-                {/* <RandomHotel /> */}
+                <button onClick={() => getRandomHotels(markPlace, setMarkHotels)}>
+                    Click For Random Hotel
+                </button>
+
             </div>
             <div className="favorite-place-container">
                 <h2>Jing's Suggestion:</h2>
@@ -51,7 +91,10 @@ export default function FavoritePlaces() {
                 <h4>Why He Loves it There:</h4>
                 <span>"{jingPlace.testimonial}"</span>
                 <h4>Featured Accomodation:</h4>
-                {/* <RandomHotel /> */}
+                <button
+                    onClick={() => getRandomHotels(jingPlace, setJingHotels)}>
+                    Click For Random Hotel
+                </button>
             </div>
             <div className="favorite-place-container">
                 <h2>Tiras's Suggestion:</h2>
@@ -59,7 +102,10 @@ export default function FavoritePlaces() {
                 <h4>Why He Loves it There:</h4>
                 <span>"{tirasPlace.testimonial}"</span>
                 <h4>Featured Accomodation:</h4>
-                {/* <RandomHotel /> */}
+                <button
+                    onClick={() => getRandomHotels(tirasPlace, setTirasHotels)}>
+                    Click For Random Hotel
+                </button>
             </div>
         </div>
     )
