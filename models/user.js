3742ca512404a0955924d2c373bd52d02d4b2d16
yonatankumsa <guitaristyona@gmail.com>
2022-12-02 20:54:11 -0500

    JWT auth and User model finished

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt')
+
+const SALT_ROUNDS = 6;
 
 const userSchema = new Schema({
     name: {type: String, required: true},
@@ -16,6 +19,22 @@ const userSchema = new Schema({
       minLength: 3,
       required: true
     }
+  }, {
+      timestamps: true,
+      toJSON: {
+          transform: function(doc, ret) {
+              delete ret.password
+              return ret
+          }
+      }
+  });
+
+  userSchema.pre('save', async function(next) {
+    // 'this' is the user doc
+    if (!this.isModified('password')) return next();
+    // update the password with the computed hash
+    this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
+    return next();
   });
 
 module.exports = mongoose.model('User', userSchema);
