cf1f4aee27829bed7c94442da4cec86e390640f6
yonatankumsa <guitaristyona@gmail.com>
2022-12-02 20:54:12 -0500

    Basic search data comes from SearchBar submit

--- src/pages/AccountPage/AccountPage.jsx
+++ src/pages/AccountPage/AccountPage.jsx
@@ -1,6 +1,9 @@
+import SearchBar from "../../components/SearchBar/SearchBar"
+
 export default function AccountPage({ user }) {
     return (
         <>
+            <SearchBar />
             <h1>Greetings, {user.name}</h1>
             <h3>Your Previous Trips:</h3>
             <h3>Edit Your Information</h3>
