14e6a1302c0943fc61301b1126bfa474c2ec1406
yonatankumsa <guitaristyona@gmail.com>
2022-12-02 20:54:12 -0500

    google places api work

--- src/components/HotelListCard/HotelListCard.jsx
+++ src/components/HotelListCard/HotelListCard.jsx
@@ -1,9 +1,33 @@
+import "./HotelListCard.css";
+import axios from "axios";
+
 export default function HotelListCard({ hotel }) {
+  const handleClick = () => {
+    const options = {
+      method: "GET",
+      url: "https://booking-com.p.rapidapi.com/v1/hotels/data",
+      params: { hotel_id: hotel.hotel_id, locale: "en-gb" },
+      headers: {
+        "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+        "X-RapidAPI-Host": process.env.X_RapidAPI_Host,
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
   return (
-    <div>
+    <div onClick={handleClick}>
       <h2>{hotel.hotel_name}</h2>
       <h3>Review Score: {hotel.review_score}</h3>
-      <img src={hotel.main_photo_url} alt="" />
+      <img src={hotel.max_photo_url} alt="" />
     </div>
   );
 }
