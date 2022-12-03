36bb2bdb07435977163be5a74781b8fc1a759656
yonatankumsa <guitaristyona@gmail.com>
2022-12-02 20:54:11 -0500

    HotelsIndexPage and IndexPage routes / page skeletons created

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,8 +2,8 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import { useState } from 'react';
 import AuthPage from '../AuthPage/AuthPage'
-import NewOrderPage from '../NewOrderPage/NewOrderPage'
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage'
+import IndexPage from '../IndexPage/IndexPage'
+import HotelsIndexPage from '../HotelsIndexPage/HotelsIndexPage'
 import NavBar from '../../components/NavBar/NavBar';
 import { getUser } from '../../utilities/users-service'
 
@@ -18,8 +18,8 @@ export default function App() {
       <NavBar user={user} setUser={setUser} />
       <Routes>
         {/* Route components in here */}
-        <Route path="/orders" element={<OrderHistoryPage />} />
-        <Route path="/orders/new" element={<NewOrderPage />} />
+        <Route path="/search" element={<IndexPage />} />
+        <Route path="/hotels" element={<HotelsIndexPage />} />
       </Routes>
       </>
       :
