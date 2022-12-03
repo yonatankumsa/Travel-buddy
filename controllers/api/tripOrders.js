957e94367c4a521df51b4a42000d9f3302ee0112
yonatankumsa <guitaristyona@gmail.com>
2022-12-02 20:54:12 -0500

    tripOrders cart, addToCart, checkout, and history are in controllers/api

--- controllers/api/tripOrders.js
+++ controllers/api/tripOrders.js
@@ -1,6 +1,38 @@
 const TripOrder = require('../../models/tripOrder')
 
 module.exports = {
+    cart,
+    addToCart,
     checkout,
     history
-}
+  };
+  
+  // A cart is the unpaid order for a user
+  async function cart(req, res) {
+    const cart = await tripOrder.getCart(req.user._id);
+    res.json(cart);
+  }
+  
+  // Add an item to the cart
+  async function addToCart(req, res) {
+    const cart = await tripOrder.getCart(req.user._id);
+    await cart.addHotelToCart(req.params.id);
+    res.json(cart);
+  }
+
+// Update the cart's isPaid property to true
+async function checkout(req, res) {
+    const cart = await TripOrder.getCart(req.user._id);
+    cart.isPaid = true;
+    await cart.save();
+    res.json(cart);
+  }
+
+  // Return the logged in user's paid order history
+async function history(req, res) {
+    // Sort most recent orders first
+    const tripOrders = await tripOrder
+      .find({ user: req.user._id, isPaid: true })
+      .sort('-updatedAt').exec();
+    res.json(tripOrders);
+  }
