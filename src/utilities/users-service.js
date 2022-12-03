cfe93c609e6464838fce4105b85653f5f64384ee
yonatankumsa <guitaristyona@gmail.com>
2022-12-02 20:54:11 -0500

    Return user for log-in form function

--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -14,6 +14,7 @@ export async function signUp(userData) {
 export async function login(credentials) {
     const token = await usersAPI.login(credentials)
     localStorage.setItem('token', token)
+    return getUser()
 }
 
 export function getToken() {
